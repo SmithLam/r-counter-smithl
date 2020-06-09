500795d97a8b8dc45acb7d5bc21816e97abd15ca
SmithLam <lamphuoctuong@gmail.com>
2020-06-09 17:06:46 +0700

    Commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,41 +1,4 @@
 //Redux method
-import React,{useEffect, useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-
-export default function Box(props) {
-    let count = useSelector(state=>state.count)
-    let boxcount = useSelector(state=>state.boxcount)
-    let colorAll = useSelector(state=>state.colorAll)
-    let colorEach = useSelector(state=>state.colorEach)
-    let index = useSelector (state=>state.index)
-    let dispatch = useDispatch()
-
-    let [color, setColor] = useState(null)
-
- 
-    //useEffect
-    useEffect(()=>{
-        if (colorAll != null && index != props.id){
-            setColor(colorAll)
-        }
-        if (colorAll != null && index == props.id){
-            setColor(colorEach)
-        }
-    },[colorAll, colorEach]) ;
-  
-
-
-    return (
-        <div>
-        <div id="box" style={{ backgroundColor: color}} ><center><p>This box is amazing!</p>
-       <p><input type="text" onChange={event => {dispatch({type:"change-each-color", payload:({index:props.id, color:event.target.value})})}}  /></p></center></div>
-        </div>
-    )
-}
-//Redux method
-
-
-//useState method
 // import React,{useEffect, useState} from 'react'
 // import {useSelector, useDispatch} from 'react-redux'
 
@@ -44,6 +7,7 @@ export default function Box(props) {
 //     let boxcount = useSelector(state=>state.boxcount)
 //     let colorAll = useSelector(state=>state.colorAll)
 //     let colorEach = useSelector(state=>state.colorEach)
+//     let index = useSelector (state=>state.index)
 //     let dispatch = useDispatch()
 
 //     let [color, setColor] = useState(null)
@@ -51,19 +15,55 @@ export default function Box(props) {
  
 //     //useEffect
 //     useEffect(()=>{
-//         if (colorAll != null){
+//         if (colorAll != null && index != props.id){
 //             setColor(colorAll)
 //         }
-//     },[colorAll]) ;
+//         if (index == props.id){
+//             setColor(colorEach)
+//         }
+//     },[colorAll, colorEach]) ;
   
 
 
 //     return (
 //         <div>
 //         <div id="box" style={{ backgroundColor: color}} ><center><p>This box is amazing!</p>
-//        <p><input type="text" onChange={event => setColor(event.target.value)}/></p></center></div>
-
+//        <p><input type="text" onChange={event => {dispatch({type:"change-each-color", payload:({index:props.id, color:event.target.value})})}}  /></p></center></div>
 //         </div>
 //     )
 // }
+//Redux method
+
+
+//useState method
+import React,{useEffect, useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+export default function Box(props) {
+    let count = useSelector(state=>state.count)
+    let boxcount = useSelector(state=>state.boxcount)
+    let colorAll = useSelector(state=>state.colorAll)
+    let colorEach = useSelector(state=>state.colorEach)
+    let dispatch = useDispatch()
+
+    let [color, setColor] = useState(null)
+
+ 
+    //useEffect
+    useEffect(()=>{
+        if (colorAll != null){
+            setColor(colorAll)
+        }
+    },[colorAll]) ;
+  
+
+
+    return (
+        <div>
+        <div id="box" style={{ backgroundColor: color}} ><center><p>This box is amazing!</p>
+       <p><input type="text" onChange={event => setColor(event.target.value)}/></p></center></div>
+
+        </div>
+    )
+}
 //useState method
